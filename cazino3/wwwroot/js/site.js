7051fbca2e4941c57dd84ada311b7304ee17b19b
Jakub Šišák <j_sisak@utb.cz>
2023-11-28 00:33:19 +0100

    just basic rolling lucky wheel without stoping.

--- cazino3/wwwroot/js/site.js
+++ cazino3/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+const svgWidth = 2500;
+const rectWidth = 100;
+const moveDistance = 3;
+
+const rects = document.querySelectorAll("rect");
+
+function moveRectangles() {
+    rects.forEach((rect) => {
+        let currentX = parseInt(rect.getAttribute('x'));
+
+        if (currentX + rectWidth > svgWidth) {
+            rect.setAttribute('x', - rectWidth);
+        } else {
+            const newX = currentX + moveDistance;
+            rect.setAttribute('x', newX);
+        }
+    });
+}
+
+// Continuously move rectangles in a smooth loop
+function animateFlow() {
+    requestAnimationFrame(animateFlow);
+    moveRectangles();
+}
+
+function calculateResult() {
+
+}
+
+// Start the animation
+animateFlow();
